8df0ce9d4b2b9923bbbf316a5c239066d0f77be1
shivendra143 <shivendrachaudhary05@gmail.com>
2018-06-28 22:06:00 +0530

    Update environment.ts
    changes

--- Jhai/src/environments/environment.ts
+++ Jhai/src/environments/environment.ts
@@ -5,13 +5,6 @@
 
 
 export const environment = {
-    production: false,
-    //APIURL:"http://activedev.westus.cloudapp.azure.com/api/v1/",
-    //APIBaseUrl:'http://activedev.westus.cloudapp.azure.com',
-    APIBaseUrl: 'http://activestaging.westindia.cloudapp.azure.com',
-    APIURL: 'http://activestaging.westindia.cloudapp.azure.com/api/v1/',
-    // APIURL: 'http://localhost:8080/api/v1/',
-    // APIBaseUrl: 'http://localhost:8080',
-    socketPort: 8080
+   
 };
 
